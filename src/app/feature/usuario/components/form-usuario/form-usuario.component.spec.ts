0ed7acfe2ec94ffb83939b271d21d6f16a12722c
Leoneider <leoneider.trigos@ceiba.com.co>
2021-10-08 10:37:13 -0500

    se agrega test en formulario

--- src/app/feature/usuario/components/form-usuario/form-usuario.component.spec.ts
+++ src/app/feature/usuario/components/form-usuario/form-usuario.component.spec.ts
@@ -9,10 +9,9 @@ describe('FormUsuarioComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FormUsuarioComponent ],
-      schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA]
-    })
-    .compileComponents();
+      declarations: [FormUsuarioComponent],
+      schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -30,6 +29,24 @@ describe('FormUsuarioComponent', () => {
     component.ngOnInit();
     fixture.detectChanges();
     expect(spy).toHaveBeenCalled;
+  });
+
+  it('se contruye formulario en el ngOnInit con usuario', () => {
+    const spy = spyOn(component, 'buildFormUsusario').and.callThrough();
+    component.usuario = {
+      id: 1,
+      documento: '1091661577',
+      nombres: 'leoneider',
+      apellidos: 'trigos',
+      celular: '3174638521',
+      email: 'leoneider@hotmail.com',
+      fehca_nacimiento: '24-16-1989',
+      contrasena: '123456',
+    };
+    component.ngOnInit();
+    fixture.detectChanges();
+    expect(spy).toHaveBeenCalled;
+    expect(component.usuarioForm.get('documento').value).toEqual('1091661577');
   });
 
   it('El formulario es valido', () => {
@@ -59,7 +76,4 @@ describe('FormUsuarioComponent', () => {
     let userData = component.getDataUsuarioForm();
     expect(userData.nombres).toEqual('leoneider');
   });
-
-
-
 });
