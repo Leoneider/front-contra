4bd4b16cd8d979d645a27df65e82b96ceca6b6ce
Leoneider <leoneider.trigos@ceiba.com.co>
2021-10-13 15:12:32 -0500

    test guard caso permitido 1

--- src/app/core/guard/security.guard.spec.ts
+++ src/app/core/guard/security.guard.spec.ts
@@ -20,12 +20,13 @@ describe('AuthGuard', () => {
   });
 
   it('No permite el paso', () => {
+    localStorage.clear();
     let res = guard.canActivate();
     expect(res).not.toBeTruthy();
   });
 
   it('Permite el paso', () => {
-    localStorage.setItem('user', '{"id":1,"documento":"1091661577","nombres":"leoneider","apellidos":"trigos guerrero","celular":"3174638521","email":"leoneider@hotmail.es","fehca_nacimiento":"24-06-1989","contrasena":"1234567"}')
+    localStorage.setItem('user', '{"id":1,"documento":"1091661577","nombres":"leoneider","apellidos":"trigos guerrero","celular":"3174638521","email":"leoneider@hotmail.es","fehca_nacimiento":"24-06-1989","contrasena":"1234567"}');
     let res = guard.canActivate();
     expect(res).toBeTruthy();
   });
--- src/app/feature/usuario/components/perfil/perfil.component.spec.ts
+++ src/app/feature/usuario/components/perfil/perfil.component.spec.ts
@@ -23,6 +23,7 @@ describe('PerfilComponent', () => {
     fixture = TestBed.createComponent(PerfilComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    localStorage.setItem('user', '{"id":1,"documento":"1091661577","nombres":"leoneider","apellidos":"trigos guerrero","celular":"3174638521","email":"leoneider@hotmail.es","fehca_nacimiento":"24-06-1989","contrasena":"1234567"}');
   });
 
   it('should create', () => {
