1bad4fd6b78e538c752ee37a5e7548f8c87a82db
Leoneider <leoneider.trigos@ceiba.com.co>
2021-10-13 15:04:42 -0500

    test guard caso permitido

--- src/app/core/guard/security.guard.spec.ts
+++ src/app/core/guard/security.guard.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { HomeComponent } from '@home/home.component';
 import { SecurityGuard } from './security.guard';
 
 describe('AuthGuard', () => {
@@ -7,13 +8,26 @@ describe('AuthGuard', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule]
+      imports: [  RouterTestingModule.withRoutes([
+        { path: 'home', component: HomeComponent },
+      ]),]
     });
     guard = TestBed.inject(SecurityGuard);
   });
 
   it('should be created', () => {
     expect(guard).toBeTruthy();
+  });
+
+  it('No permite el paso', () => {
+    let res = guard.canActivate();
+    expect(res).not.toBeTruthy();
+  });
+
+  it('Permite el paso', () => {
+    localStorage.setItem('user', '{"id":1,"documento":"1091661577","nombres":"leoneider","apellidos":"trigos guerrero","celular":"3174638521","email":"leoneider@hotmail.es","fehca_nacimiento":"24-06-1989","contrasena":"1234567"}')
+    let res = guard.canActivate();
+    expect(res).toBeTruthy();
   });
 
 
